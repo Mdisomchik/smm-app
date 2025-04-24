04061861503d1618d63dead9c2b06a7cf178e806
mdisomchik <maximcapinus@gmail.com>
2025-04-25 02:54:53 +0300

    somethign wrong with credentials, gg

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,25 +33,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     );
   }
 
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
-      <View style={styles.container}>
-        <Slot />
-      </View>
-    </AuthContext.Provider>
-  );
-};
-
-// Custom hook to use the Auth Context
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
-};
-
-// Root Layout Component
 export default function RootLayout() {
   const { isAuthenticated } = useAuth();
   const router = useRouter();
